d209235d388958cc4e64aed30dbfe9254a03cf20
namle24 <nma2409.na@gmail.com>
2024-09-06 13:10:49 +0700

    Update Prac5

--- USTH WEATHER/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTH WEATHER/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -1,25 +1,40 @@
 package vn.edu.usth.weather;
 
+import static android.app.ProgressDialog.show;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ForecastFragment extends Fragment {
 
+    ImageButton refresh;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
 
-
+        refresh = (ImageButton) view.findViewById(R.id.refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Refresh", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
+
+
 }
--- USTH WEATHER/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTH WEATHER/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,21 +1,30 @@
 package vn.edu.usth.weather;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WeatherActivity extends AppCompatActivity {
 
+    ImageButton refresh;
     private static final String TAG = "WeatherActivity";
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        setContentView(R.layout.fragment_forecast);
         Log.i(TAG, "onCreate called");
 
         ForecastFragment firstFragment = new ForecastFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+
     }
 
     @Override
