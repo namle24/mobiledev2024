71f6399fbb5a5bba61705aa2292fd96f9fc30400
namle24 <nma2409.na@gmail.com>
2024-09-14 08:57:10 +0700

    Prac 9

--- USTH WEATHER/app/src/main/java/vn/edu/usth/weather/HaNoiFragment.java
+++ USTH WEATHER/app/src/main/java/vn/edu/usth/weather/HaNoiFragment.java
@@ -3,6 +3,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -10,7 +11,8 @@ public class HaNoiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.hanoi_weather, container, false);
+        View view = inflater.inflate(R.layout.hanoi_weather, container, false);
+        return view;
     }
 
     public static HaNoiFragment newInstance() {
